68d1535945b3da38fbe3b1f829d4a4d7c99f33e8
zhaozhentao <344696734@qq.com>
2018-05-25 18:04:31 +0800

    router work

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,14 @@
 <template>
   <div id="app">
-    <Header/>
+    <router-view/>
   </div>
 </template>
 
 <script>
-  import Header from '@/components/layouts/Header';
 
   export default {
     name: 'App',
     components: {
-      Header
     }
   }
 </script>
---
+++ src/pages/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <Header/>
+</template>
+
+<script>
+  import Header from '@/components/layouts/Header';
+
+  export default {
+    name: "Home",
+    components: {
+      Header
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
