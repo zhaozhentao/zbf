762a0298838121e33ab23448d864e87f20897d3a
zhaozhentao <344696734@qq.com>
2018-05-25 14:48:10 +0800

    起步

--- src/App.vue
+++ src/App.vue
@@ -6,23 +6,26 @@
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+  import HelloWorld from './components/HelloWorld'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  export default {
+    name: 'App',
+    components: {
+      HelloWorld
+    }
   }
-}
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss">
+  $icon-font-path: "~bootstrap-sass/assets/fonts/bootstrap/";
+  @import "~bootstrap-sass/assets/stylesheets/_bootstrap";
+
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
 </style>
